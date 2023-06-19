bdaddedafefa27140890cfecaf8da136b28d77ca
nathansabin <sabinnat001@gmail.com>
2023-06-18 21:33:00 -0700

    add models

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    Product.findAll({include: [{model: Category}]})
+    .then((productData) => {
+      res.status(200).json(productData);
+    })
+  } catch(err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    Product.findByPk(req.params.id, )
+    .then((productData) => {
+      res.status(200).json(productData);
+    });
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -94,6 +111,23 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  // trys to remove product
+  try {
+    Product.destroy(
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+    .then((removedProduct) => {
+      res.status(200).json(removedProduct);
+    });
+  } 
+  // errors if unsuccessful
+  catch(err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,18 +3,31 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await ProductTag.findAll({include: [{model: Product}, {model: Tag}]});
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await ProductTag.findByPk(req.params.id, {include: [{model: Product}, {model: Tag}]});
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  
 });
 
 router.put('/:id', (req, res) => {
